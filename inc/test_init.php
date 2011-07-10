d2e4311dad4a8927579cbdb0fb48dcf3eb7c6919
c9s <cornelius.howl@gmail.com>
2011-07-10 18:47:24 +0800

    Remove inc/test_init , Update unit test files

--- inc/test_init.php
+++
@@ -1,6 +0,0 @@
-<?php
-define( 'PH_ROOT' , dirname(__DIR__) );
-define( 'PH_ENVIRONMENT' , 'dev' ); // or 'prod'
-define( 'PH_DEBUG', false);
-define( 'PH_APP_NAME', 'Test' );
-require_once( 'autoload.php' );
