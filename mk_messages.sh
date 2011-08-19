0820e46052f7e0b67f88d4a1c6ba0c97d5155f8e
c9s <cornelius.howl@gmail.com>
2011-08-19 21:04:20 +0800

    update message script

--- mk_messages.sh
+++ mk_messages.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # find plugins core app lib -iname "*.php" -exec xgettext --from-code=UTF-8 -n --omit-header -L PHP -j -o locale/phifty.pot {} \;
 find plugins tests dists/EteDB cache core lib -iname "*.php" -exec xgettext --from-code=UTF-8 -n -L PHP -j -o locale/phifty.pot {} \;
+
+
+f=locale/zh_TW/LC_MESSAGES/phifty.po
+msgmerge $f locale/phifty.pot >| tmp.po
+cp tmp.po $f
+open $f
+po update
