2cad4a8e874eea5a9ef0ceafa3792fce768cad02
c9s <cornelius.howl@gmail.com>
2011-08-19 21:00:33 +0800

    Update locale

--- mk_messages.sh
+++ mk_messages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # find plugins core app lib -iname "*.php" -exec xgettext --from-code=UTF-8 -n --omit-header -L PHP -j -o locale/phifty.pot {} \;
-find plugins core app lib -iname "*.php" -exec xgettext --from-code=UTF-8 -n -L PHP -j -o locale/phifty.pot {} \;
+find plugins tests dists/EteDB cache core lib -iname "*.php" -exec xgettext --from-code=UTF-8 -n -L PHP -j -o locale/phifty.pot {} \;
