5ad77f41f13aa56582bbdc1c9e0b0cbe299e3278
c9s <cornelius.howl@gmail.com>
2011-09-04 01:52:22 +0800

    passing params to backend.

--- testapp/lib/TestApp.php
+++ testapp/lib/TestApp.php
@@ -4,6 +4,7 @@ class TestApp extends \Phifty\MicroApp
 {
     function init()
     {
+        $this->route( '/html5upload' , array( 'controller' => '\TestApp\Controller\Html5Upload' ) );
         $this->route( '/foo' , array( 'template' => array( 
             'template' => 'foo.html',
             'engine' => 'twig' )) 
