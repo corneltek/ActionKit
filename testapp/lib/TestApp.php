94373b106bff0ba73e147ed48e2f7b77f216c468
c9s <cornelius.howl@gmail.com>
2011-09-29 19:47:28 +0800

    Use twig config (Twig View)

--- testapp/lib/TestApp.php
+++ testapp/lib/TestApp.php
@@ -8,6 +8,7 @@ class TestApp extends \Phifty\MicroApp
             'template' => 'foo.html',
             'engine' => 'twig' )) 
         );
+        $this->route( '/twig_test' , array( 'template' => array( 'template' => 'twig_test.html' , 'engine' => 'twig' ) ) );
         $this->route( '/bar' , 'Bar' );
     }
 }
