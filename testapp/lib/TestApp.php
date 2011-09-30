9d66bfa607b10ce59f3ce8dd1ba84d1618a82e56
c9s <cornelius.howl@gmail.com>
2011-10-01 01:35:05 +0800

    Add WidgetLoader

--- testapp/lib/TestApp.php
+++ testapp/lib/TestApp.php
@@ -10,6 +10,7 @@ class TestApp extends \Phifty\MicroApp
         );
         $this->route( '/twig_test' , array( 'template' => array( 'template' => 'twig_test.html' , 'engine' => 'twig' ) ) );
         $this->route( '/bar' , 'Bar' );
+        $this->route( '/galleria_demo' , 'GalleriaDemo' );
     }
 }
 
--- widgets/Galleria/Galleria.php
+++ widgets/Galleria/Galleria.php
@@ -6,14 +6,22 @@ class Galleria
 {
     function js() 
     {
-
+        return array(
+            "galleria-1.2.5.js"
+        );
     }
 
     function css() 
     {
-
+        return array(
+            "themes/twelve/galleria.twelve.css"
+        );
     }
 
+    function template()
+    {
+
+    }
 }
 
 
