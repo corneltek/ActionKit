52df81f4dee2d2835b46781650e9ea8423d10e58
c9s <cornelius.howl@gmail.com>
2011-10-01 18:33:22 +0800

    Export widget loader

--- widgets/Galleria/Galleria.php
+++ widgets/Galleria/Galleria.php
@@ -5,22 +5,42 @@ use Phifty\Widget;
 
 class Galleria extends Widget
 {
+	public $width = 400;
+	public $height = 400;
+	public $images = array();
+
     function js() 
     {
-        return array(
-            "galleria-1.2.5.js"
-        );
+        return array( "galleria-1.2.5.js" );
     }
 
     function css() 
     {
-        return array(
-            "themes/twelve/galleria.twelve.css"
-        );
+        return array( "themes/twelve/galleria.twelve.css" );
     }
 
-    function template()
+	function addImage( $path , $attrs = array() ) 
+	{
+		$this->images[] = array( 'path' => $path, 'attrs' => $attrs );
+	}
+
+    function defaultTemplate()
     {
+		return <<<"HTML"
+<script type="text/javascript">
+jQuery(document.body).ready(function() {
+	Galleria.loadTheme('{{ Widget.baseUrl }}/themes/twelve/galleria.twelve.min.js');
+	jQuery('#gallery').height( 500 );
+	jQuery('#gallery').galleria();
+
+});
+</script>
+<div id="gallery">
+{% for image in Widget.images %}
+	<img src="{{ image.path }}"/>
+{% endfor %}
+</div>
+HTML;
 
     }
 }
