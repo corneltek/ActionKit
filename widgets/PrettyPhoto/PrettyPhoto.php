bddb5107b9208e87da8eeb1541e9e5463cca8468
c9s <cornelius.howl@gmail.com>
2011-10-02 00:28:55 +0800

    Adjust style.

--- widgets/PrettyPhoto/PrettyPhoto.php
+++ widgets/PrettyPhoto/PrettyPhoto.php
@@ -36,17 +36,6 @@ class PrettyPhoto extends Widget
 	function template()
 	{
 		return <<<"HTML"
-<!--
-<ul class="prettyphoto clearfix">
-    <li><a href="{{ Widget.baseUrl }}/images/fullscreen/1.jpg?lol=lol" rel="prettyPhoto[gallery1]" title="Caption"><img src="{{ Widget.baseUrl }}/images/thumbnails/t_1.jpg" width="60" height="60" alt="Red round shape" /></a></li>
-    <li><a href="{{ Widget.baseUrl }}/images/fullscreen/2.jpg" rel="prettyPhoto[gallery1]"><img src="{{ Widget.baseUrl }}/images/thumbnails/t_2.jpg" width="60" height="60" alt="Nice building" /></a></li>
-    <li><a href="{{ Widget.baseUrl }}/images/fullscreen/3.jpg" rel="prettyPhoto[gallery1]"><img src="{{ Widget.baseUrl }}/images/thumbnails/t_3.jpg" width="60" height="60" alt="Fire!" /></a></li>
-    <li><a href="{{ Widget.baseUrl }}/images/fullscreen/4.jpg" rel="prettyPhoto[gallery1]"><img src="{{ Widget.baseUrl }}/images/thumbnails/t_4.jpg" width="60" height="60" alt="Rock climbing" /></a></li>
-    <li><a href="{{ Widget.baseUrl }}/images/fullscreen/5.jpg" rel="prettyPhoto[gallery1]"><img src="{{ Widget.baseUrl }}/images/thumbnails/t_5.jpg" width="60" height="60" alt="Fly kite, fly!" /></a></li>
-    <li><a href="{{ Widget.baseUrl }}/images/fullscreen/6.jpg" rel="prettyPhoto[gallery1]"><img src="{{ Widget.baseUrl }}/images/thumbnails/t_2.jpg" width="60" height="60" alt="Nice building" /></a></li>
-</ul>
--->
-
 <style>
 ul.prettyphoto li { 
     display: inline;
@@ -54,7 +43,6 @@ ul.prettyphoto li {
 }
 </style>
 <ul class="prettyphoto">
-
     {% for item in Widget.images %}
         <li>
             <a href="{{ item.image }}" 
