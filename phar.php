c15d9cea9725b1cae0ee08f28f589407a1e22ff0
c9s <cornelius.howl@gmail.com>
2011-10-27 01:48:38 +0800

    phar.php script.

--- phar.php
+++ phar.php
@@ -1,7 +1,8 @@
 <?php
 $archive = new Phar('twig.phar', 0, 'twig.phar');
 $archive->setStub('<?php
-Phar::mapPhar();
-__HALT_COMPILER();
+# Phar::mapPhar();
+# __HALT_COMPILER();
 ');
 $archive->buildFromDirectory('dists/Twig/lib');
+
