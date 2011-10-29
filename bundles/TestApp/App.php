b993a384c21af5375f144a11da1eeebeecf33d8a
c9s <cornelius.howl@gmail.com>
2011-10-29 14:29:30 +0800

    Update bundles for TestApp

---
+++ bundles/TestApp/App.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+class App extends \Phifty\MicroApp
+{
+    function init()
+    {
+        $this->route( '/foo' , array( 'template' => array( 
+            'template' => 'App/template/foo.html',
+            'engine' => 'twig' )) 
+        );
+        $this->route( '/twig_test' , array( 'template' => array( 'template' => 'App/template/twig_test.html' , 'engine' => 'twig' ) ) );
+        $this->route( '/bar' , 'Bar' );
+        $this->route( '/galleria_demo' , 'GalleriaDemo' );   // Controller\GalleriaDemo
+        $this->route( '/pretty_photo' , 'PrettyPhoto' );     // Controller\PrettyPhoto
+    }
+}
+
+?>
---
+++ bundles/TestApp/TestApp.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TestApp;
+
+class TestApp extends \Phifty\MicroApp
+{
+    function init()
+    {
+        $this->route( '/foo' , array( 'template' => array( 
+            'template' => 'TestApp/template/foo.html',
+            'engine' => 'twig' )) 
+        );
+        $this->route( '/twig_test' , array( 'template' => array( 'template' => 'App/template/twig_test.html' , 'engine' => 'twig' ) ) );
+        $this->route( '/bar' , 'Bar' );
+        $this->route( '/galleria_demo' , 'GalleriaDemo' );   // Controller\GalleriaDemo
+        $this->route( '/pretty_photo' , 'PrettyPhoto' );     // Controller\PrettyPhoto
+    }
+}
+
