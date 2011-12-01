b52dca07b4d94e206336b78ecfa434f4e02f30d0
c9s <cornelius.howl@gmail.com>
2011-12-02 03:13:00 +0800

    Update tinymce helper

--- bundles/Core/web/js/tinymce_helper.js
+++ bundles/Core/web/js/tinymce_helper.js
@@ -20,7 +20,7 @@ tinyMCE_Helper = {
                 editor_selector : "mceEditor",
 
                 // Theme options
-                theme_advanced_buttons1 : "formatselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,table,removeformat,code,|,image,link,|,preview,imagechooser",
+                theme_advanced_buttons1 : "formatselect,fontselect,fontsizeselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,table,removeformat,code,|,image,link,|,preview,imagechooser",
                 theme_advanced_buttons2 : "",
                 theme_advanced_buttons3 : "",
                 theme_advanced_buttons4 : "",
@@ -32,21 +32,7 @@ tinyMCE_Helper = {
 
                 // default language
                 language : "zh-tw",
-
                 height : "480px",
-
-                // Style formats
-                /*
-                style_formats : [
-                        {title : 'Bold text', inline : 'b'},
-                        {title : 'Red text', inline : 'span', styles : {color : '#ff0000'}},
-                        {title : 'Red header', block : 'h1', styles : {color : '#ff0000'}},
-                        {title : 'Example 1', inline : 'span', classes : 'example1'},
-                        {title : 'Example 2', inline : 'span', classes : 'example2'},
-                        {title : 'Table styles'},
-                        {title : 'Table row 1', selector : 'tr', classes : 'tablerow1'}
-                ],*/
-
                 formats : {
                         alignleft : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'left'},
                         aligncenter : {selector : 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img', classes : 'center'},
@@ -59,6 +45,7 @@ tinyMCE_Helper = {
                         customformat : {inline : 'span', styles : {color : '#00ff00', fontSize : '20px'}, attributes : {title : 'My custom format'}}
                 },
 
+                // Example content CSS (should be your site CSS)
                 content_css : "/ph/Core/js/tinymce_content.css",
 
                 // Skin options
@@ -67,7 +54,6 @@ tinyMCE_Helper = {
                 convert_urls : false
 
                 // document_base_url : "/ph/Core/js/tinymce/"
-                // Example content CSS (should be your site CSS)
         }
     } 
 };
