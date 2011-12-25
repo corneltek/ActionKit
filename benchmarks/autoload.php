c02b44129f19361e70a069be67e0ac0abd8e22bd
c9s <cornelius.howl@gmail.com>
2011-12-25 21:10:47 +0800

    Log benchmarks

---
+++ benchmarks/autoload.php
@@ -0,0 +1,17 @@
+<?php
+$taskName = 'autoload';
+
+$t1 = microtime(true);
+require 'autoload.php';
+$t2 = (microtime(true) - $t1);
+echo $t2 * 1000 . " milliseconds.\n";
+echo $t2 . " seconds.\n";
+
+$m = new Mongo;
+$db = $m->benchmarks;
+// select a collection (analogous to a relational database's table)
+$collection = $db->phifty;
+
+#  // add a record
+$obj = array( 'task' => $taskName , 'duration' => $t2 * 1000 , 'unit' => 'ms' , 'created_on' => new MongoDate(time()) ); // microseconds
+$collection->insert($obj);
