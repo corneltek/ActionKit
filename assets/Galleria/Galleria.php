ca77d891e9d1a66d0cf09eb56da1d9dbf2301efd
c9s <cornelius.howl@gmail.com>
2011-12-25 19:11:22 +0800

    Clean up widget stuff.

--- assets/Galleria/Galleria.php
+++ assets/Galleria/Galleria.php
@@ -1,25 +1,10 @@
 <?php
 
 namespace Phifty\Assets;
-
 use Phifty\Asset\Asset;
 
 class Galleria extends Asset
 {
-	public $width = 400;
-	public $height = 400;
-	public $images = array();
-
-    function height( $px )
-    {
-        $this->height = $px;
-    }
-
-    function width( $px )
-    {
-        $this->width = $px;
-    }
-
     function js() 
     {
         return array( "galleria-1.2.5.js" );
@@ -29,34 +14,4 @@ class Galleria extends Asset
     {
         return array( "themes/twelve/galleria.twelve.css" );
     }
-
-	function addImage( $path , $attrs = array() ) 
-	{
-		$this->images[] = array( 'path' => $path, 'attrs' => $attrs );
-	}
-
-    function template()
-    {
-		return <<<"HTML"
-
-	<script type="text/javascript">
-	jQuery(document.body).ready(function() {
-		Galleria.loadTheme('{{ Asset.baseUrl }}/themes/twelve/galleria.twelve.min.js');
-		jQuery('#gallery').height( {{ Asset.height }} );
-		jQuery('#gallery').galleria();   
-	}
-	);
-	</script>
-
-	<div id="gallery">
-	{% for image in Asset.images %}
-		<img src="{{ image.path }}"/>
-	{% endfor %}
-	</div>
-
-HTML;
-
-    }
 }
-
-
