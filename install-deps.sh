99ce038a150c5175c70b1d3a13f6c12b7d070985
c9s <cornelius.howl@gmail.com>
2012-03-19 20:41:34 +0800

    Add install-deps.sh

---
+++ install-deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pear channel-discover pear.corneltek.com
+pear channel-discover components.ez.no
+pear channel-discover pear.symfony-project.com
+pear channel-discover pear.pearplex.net
+pear channel-discover pear.phpunit.de
+pear channel-discover pear.twig-project.org
+pear channel-discover pear.firephp.org
+pear install -f -a phpunit/PHPUnit
+pear install -f -a twig/Twig
+pear install -f -a pearplex/PHPExcel
+pear install -f -a firephp/FirePHPCore
+pecl install apc
+pecl install yaml
+pecl install xdebug
