5cc6b976ab48935c573625eec5b7955763f9b5d9
c9s <cornelius.howl@gmail.com>
2012-05-29 19:55:46 +0800

    Add action generator test

--- src/ActionKit/ActionGenerator.php
+++ src/ActionKit/ActionGenerator.php
@@ -5,6 +5,7 @@ namespace ActionKit;
  * Action Generator
  *
  *    $generator = new ActionGenerator(array( 
+ *          'cache' => true,                 // this enables apc cache.
  *          'cache_dir' => 'phifty/cache', 
  *          'template_dirs' => array( 'Resource/Templates' )
  *    ));
@@ -17,17 +18,43 @@ namespace ActionKit;
  */
 class ActionGenerator
 {
+
+    public $cache;
+
     public $cacheDir;
 
-    function __construct( $options ) {
-        if( isset($options['cache_dir']) ) {
-            $this->cacheDir = $options['cache_dir'];
-        }
+    function __construct( $options = array() ) 
+    {
+        $this->cache = isset($options['cache']) && extension_loaded('apc');
     }
 
-    function generate($className, $templateName, $arguments ) {
-
+    function generateClassCode( $modelClass , $type ) {
+        $p = strpos( $modelClass , '\\' );
+        $bp = strrpos( $modelClass , '\\' );
+        $ns = substr($modelClass,0,$p);
+        $modelName = substr($modelClass, $bp + 1 );
+        return $this->generateClassCodeWithNamespace( $ns , $modelName, $type );
     }
+
+    function generateClassCodeWithNamespace( $ns , $modelName , $type )
+    {
+        $recordClass  = '\\' . $ns . '\Model\\' . $modelName;
+        $actionClass  = $type . $modelName;
+        $baseAction   = $type . 'RecordAction';
+        $code =<<<CODE
+namespace $ns\\Action {
+    use ActionKit\\RecordAction\\$baseAction;
+    class $actionClass extends $baseAction
+    {
+        public \$recordClass = '$recordClass';
+    }
+}
+namespace { return 1; }
+CODE;
+        return $code;
+    }
+
+
 }
 
 
