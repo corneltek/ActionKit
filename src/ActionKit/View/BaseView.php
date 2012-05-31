3ca964b675227358d78976877cbca53f90d5471e
c9s <cornelius.howl@gmail.com>
2012-06-01 03:08:50 +0800

    Add StackViewTest for ActionKit

--- src/ActionKit/View/BaseView.php
+++ src/ActionKit/View/BaseView.php
@@ -6,12 +6,15 @@ class BaseView
 
     public $action;
 
+    public $options;
+
     /**
      *
      * @param ActionKit\Action $action
      */
-    function __construct($action) {
+    function __construct($action, $options = array() ) {
         $this->action = $action;
+        $this->options = $options;
     }
 
     abstract function build();
--- src/ActionKit/View/StackView.php
+++ src/ActionKit/View/StackView.php
@@ -1,17 +1,56 @@
 <?php
 namespace ActionKit\View;
+use FormKit;
+
+/**
+ * Action View Synopsis
+ *
+ *      $action =  ....
+ *      $view = new ActionKit\View\StackView($action, $options );
+ *      $view->render();
+ */
 
 class StackView extends BaseView
 {
+    public $layout;
+
+
+
+    function option($key) {
+        if( isset($this->options[$key]) ) {
+            return $this->options[$key];
+        }
+    }
 
     function build()
     {
+        $this->layout = new FormKit\Layout\GenericLayout;
+        if( $width = $this->option('width') ) {
+            $layout->width( $width );
+        }
+        if( $padding = $this->option('cellpadding') ) {
+            $layout->cellpadding( $padding );
+        }
+        if( $spacing = $this->option('cellspacing') ) {
+            $layout->cellspacing( $spacing );
+        }
+        if( $border = $this->option('border') ) {
+            $layout->border(0);
+        }
 
+        // for each widget, push it into stack
+        foreach( $this->action->params as $param ) {
+            $widget = $param->createWidget();
+            $layout->addWidget( $widget );
+        }
     }
 
     function render() 
     {
-
+        $form = new FormKit\Element\Form;
+        $form->method('post');
+        $form->addChild( $layout );
+        return $form->render();
     }
 
 }
