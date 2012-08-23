bc4bad659f7407daef23ec19dc913c33d74154e6
c9s <cornelius.howl@gmail.com>
2012-08-24 00:29:28 +0800

    Import AccessRule classes

---
+++ src/Kendo/Model/AccessRuleBase.php
@@ -0,0 +1,15 @@
+<?php
+namespace Kendo\Model;
+
+
+
+class AccessRuleBase 
+extends \Phifty\Model
+{
+
+            const schema_proxy_class = '\\Kendo\\Model\\AccessRuleSchemaProxy';
+        const collection_class = '\\Kendo\\Model\\AccessRuleCollection';
+        const model_class = '\\Kendo\\Model\\AccessRule';
+        const table = 'access_rules';
+        
+}
