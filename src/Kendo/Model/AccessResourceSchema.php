076af0fcdbba318b239ce583dca2025d5c06ba41
c9s <cornelius.howl@gmail.com>
2012-08-23 21:23:02 +0800

    Add requried attribute

--- src/Kendo/Model/AccessResourceSchema.php
+++ src/Kendo/Model/AccessResourceSchema.php
@@ -6,11 +6,13 @@ class AccessResourceSchema extends SchemaDeclare
 {
     function schema() {
         $this->column('resource')
-            ->varchar(64);
+            ->varchar(64)
+            ->required();
         $this->column('resource_label')
             ->varchar(128);
         $this->column('operation')
-            ->varchar(64);
+            ->varchar(64)
+            ->required();
         $this->column('operation_label')
             ->varchar(128);
     }
