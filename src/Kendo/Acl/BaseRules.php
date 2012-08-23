7da435fa270454d8c64e66a8917793f75a5759cd
c9s <cornelius.howl@gmail.com>
2012-08-23 22:03:42 +0800

    Add BaseRules class

--- src/Kendo/Acl/BaseRules.php
+++ src/Kendo/Acl/BaseRules.php
@@ -3,8 +3,42 @@ namespace Kendo\Acl;
 
 class BaseRules
 {
-    public $rules = array();
+    public $allowRules = array();
+    public $denyRules = array();
 
+    public function add($roleId, $resourceId, $operationId, $allow )
+    {
+
+    }
+
+    public function addAllowRule( $roleId, $resourceId, $operationId ) 
+    {
+        if( ! isset($this->allowRules[ $roleId ]) ) {
+            $this->allowRules[ $roleId ] = array();
+        }
+        if( ! isset($this->allowRules[ $roleId ][ $resourceId ]) ) {
+            $this->allowRules[ $roleId ][ $resourceId ] = array();
+        }
+        $this->allowRules[$roleId][$resourceId][ $operationId ] = $allow;
+    }
+
+    public function addDenyRule( $roleId, $resourceId, $operationId )
+    {
+        if( ! isset($this->denyRules[ $roleId ]) ) {
+            $this->denyRules[ $roleId ] = array();
+        }
+        if( ! isset($this->denyRules[ $roleId ][ $resourceId ]) ) {
+            $this->denyRules[ $roleId ][ $resourceId ] = array();
+        }
+        $this->denyRules[$roleId][$resourceId][ $operationId ] = $allow;
+    }
+
+    public function export() {
+        return array(
+            'allow' => $this->allowRules,
+            'deny'  => $this->denyRules,
+        );
+    }
 
 }
 
--- src/Kendo/Model/AccessResourceSchema.php
+++ src/Kendo/Model/AccessResourceSchema.php
@@ -5,14 +5,18 @@ use LazyRecord\Schema\SchemaDeclare;
 class AccessResourceSchema extends SchemaDeclare
 {
     function schema() {
+
         $this->column('resource')
             ->varchar(64)
             ->required();
+
         $this->column('resource_label')
             ->varchar(128);
+
         $this->column('operation')
             ->varchar(64)
             ->required();
+
         $this->column('operation_label')
             ->varchar(128);
     }
