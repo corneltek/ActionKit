0f706a85ebf54c0b4c306ca38a73471ed9cff10f
c9s <cornelius.howl@gmail.com>
2012-08-23 23:25:36 +0800

    Add DatabaseBaseRules (db-based rules class)

---
+++ src/Kendo/Acl/DatabaseRules.php
@@ -0,0 +1,99 @@
+<?php
+namespace Kendo\Acl;
+use Kendo\Model\AccessResource as AR;
+use Kendo\Model\AccessResourceCollection as ARCollection;
+
+class Resource {
+
+    public $id;
+    public $label;
+
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
+    public function __toString() {
+        return $this->id;
+    }
+
+    public function toArray() {
+        return array(
+            'id' => $this->id,
+            'label' => $this->label,
+        );
+    }
+}
+
+class Operation { 
+
+    public $id;
+    public $label;
+
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
+    public function __toString() {
+        return $this->id;
+    }
+
+    public function toArray() {
+        return array(
+            'id' => $this->id,
+            'label' => $this->label,
+        );
+    }
+}
+
+class Rule {
+    public $role;
+    public $resource;
+    public $operation;
+
+    /**
+     * description
+     */
+    public $desc;
+    public $allow = false;
+
+    public function __construct($role,$resource,$operation,$allow) {
+        $this->role = $role;
+        $this->operation = new Operation($operation);
+        $this->resource = new Resource($operation);
+        $this->allow = $allow;
+    }
+
+    public function sync() {
+        // sync resource operation table
+        $ar = new AR;
+        $ar->createOrUpdate(array( 
+            'resource' => $this->resource->id,
+            'resource_label' => $this->resource->label,
+            'operation' => $this->operation->id,
+            'operation_label' => $this->operation->label,
+            'description' => $this->desc,
+        ),array('resource','operation'));
+    }
+
+    public function toArray() {
+        return array( 
+            'role' => $this->role,
+            'operation' => $this->operation->toArray(),
+            'resource' => $this->resource->toArray(),
+            'allow' => $this->allow,
+            'desc' => $this->desc,
+        );
+    }
+}
+
+/**
+ * Use access control rules from database.
+ */
+abstract class DatabaseRules extends BaseRules
+{
+
+
+
+}
+
+
