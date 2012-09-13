649ed0e7e306cc968ee1b44bd9eeafe006236267
c9s <cornelius.howl@gmail.com>
2012-09-14 00:53:50 +0800

    Add BulkAction

---
+++ src/ActionKit/RecordAction/BulkAction.php
@@ -0,0 +1,49 @@
+<?php
+namespace ActionKit\RecordAction;
+use ActionKit\Action;
+
+class BulkAction extends Action
+{
+    const TYPE = 'bulk';
+
+    public $recordClass;
+
+    public function schema() 
+    {
+        $this->param('items');
+    }
+
+    public function runValidate() 
+    {
+        if( isset( $this->args['items'] ) )
+            return false;  // no error
+        return true;
+    }
+
+    // TODO: we should use 
+    //    collection and where id in (1,2,3) to improve performance.
+    public function loadRecords() 
+    {
+        $itemIds = $this->arg('items');
+        $records = array();
+        foreach( $itemIds as $id ) {
+            $record = new $this->recordClass;
+            $record->load( (int) $id );
+            if($record->id)
+                $records[] = $record;
+        }
+        return $records;
+    }
+
+    public function run() 
+    {
+        $records = $this->loadRecords();
+        foreach( $records as $record ) {
+            $ret = $record->delete();
+        }
+        return $this->deleteSuccess($ret);
+    }
+}
+
+
+
--- src/ActionKit/RecordAction/BulkDeleteRecordAction.php
+++ src/ActionKit/RecordAction/BulkDeleteRecordAction.php
@@ -1,32 +1,17 @@
 <?php
 namespace ActionKit\RecordAction;
 
-abstract class BulkDeleteRecordAction extends DeleteRecordAction
+abstract class BulkDeleteRecordAction extends BulkAction
 {
     const TYPE = 'bulk_delete';
 
-    public $enableLoadRecord = false;
-
-    public function runValidate() 
-    {
-        if( isset( $this->args['items'] ) )
-            return false;  // no error
-        return true;
-    }
-
     public function run()
     {
-        try {
-            $items = $this->arg('items');
-            $record = $this->record;
-            foreach( $items as $id ) {
-                $record->load( (int) $id );
-                $ret = $record->delete();
-            }
-            return $this->deleteSuccess($ret);
-        } catch( Exception $e ) {
-            return $this->error( $e->getMessage() );
+        $records = $this->loadRecords();
+        foreach( $records as $record ) {
+            $ret = $record->delete();
         }
+        return $this->deleteSuccess($ret);
     }
 }
 
