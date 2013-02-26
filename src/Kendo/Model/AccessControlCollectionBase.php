184c16efc70dd7a4b9b75a2580b2ff31a0e1ffdd
c9s <cornelius.howl@gmail.com>
2013-02-26 17:45:33 +0800

    Remove Kendo from Core.

--- src/Kendo/Model/AccessControlCollectionBase.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Kendo\Model;
-
-class AccessControlCollectionBase  extends \LazyRecord\BaseCollection {
-const schema_proxy_class = '\\Kendo\\Model\\AccessControlSchemaProxy';
-const model_class = '\\Kendo\\Model\\AccessControl';
-const table = 'access_controls';
-
-}
--- src/Kendo/Model/AccessRuleCollectionBase.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Kendo\Model;
-
-class AccessRuleCollectionBase  extends \LazyRecord\BaseCollection {
-const schema_proxy_class = '\\Kendo\\Model\\AccessRuleSchemaProxy';
-const model_class = '\\Kendo\\Model\\AccessRule';
-const table = 'access_rules';
-
-}
