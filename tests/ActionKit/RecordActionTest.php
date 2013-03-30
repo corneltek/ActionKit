a975b2629f930af64434fec3207769c07ae64b7b
c9s <cornelius.howl@gmail.com>
2013-03-30 20:19:52 +0800

    Add RecordActionTest

---
+++ tests/ActionKit/RecordActionTest.php
@@ -0,0 +1,67 @@
+<?php
+use ActionKit\CRUD;
+use LazyRecord\ModelTestCase;
+
+class RecordActionTest extends ModelTestCase
+{
+    public function getModels()
+    {
+        return array( 
+            'Product\Model\ProductSchema'
+        );
+    }
+
+    public function createProduct( $name )
+    {
+        $p = new \Product\Model\Product;
+        $ret = $p->create(array( 
+            'name' => $name
+        ));
+        ok($ret->success,'record created.');
+        ok($p->id,'got created id');
+        return $p;
+    }
+
+    public function createProductActionClass($type)
+    {
+        return CRUD::generate('Product\Model\Product',$type);
+    }
+
+    public function testRecordCreate()
+    {
+        $class = $this->createProductActionClass('Create');
+        $create = new $class(array('name' => 'Foo'));
+        ok( $create->run(), 'create action returns success.' );
+
+        ok( $create->getRecord()->delete()->success );
+    }
+
+    public function testRecordUpdate()
+    {
+        $product = $this->createProduct('Book A');
+        ok($product);
+
+        $class = $this->createProductActionClass('Update');
+        $update = new $class(array('id' => $product->id , 'name' => 'Foo'));
+        ok( $update->run() );
+        $record = $update->getRecord();
+        ok($record->id);
+        is('Foo', $record->name);
+
+        $record->delete();
+    }
+
+    public function testRecordUpdateWithExistingRecordObject()
+    {
+        $product = $this->createProduct('Book A');
+        ok($product);
+
+        $class = $this->createProductActionClass('Update');
+        $update = new $class(array('name' => 'Bar'), $product);
+        ok( $update->run() );
+        $record = $update->getRecord();
+        ok($record->id);
+        is('Bar', $record->name);
+    }
+
+}
