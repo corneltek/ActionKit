cda63da54a950cdb3f8e86ae9f9494b66943fd8b
c9s <cornelius.howl@gmail.com>
2013-03-30 19:32:27 +0800

    Add RecordAction tests

--- tests/Product/ProductActionTest.php
+++ tests/Product/ProductActionTest.php
@@ -1,9 +1,59 @@
 <?php
 use LazyRecord\ModelTestCase;
+use ActionKit\CRUD;
 
+/**
+ * RecordAction
+ */
 class ProductActionTest extends ModelTestCase
 {
+    public $driver = 'sqlite';
 
+    public function getModels()
+    {
+        return array( 'Product\\Model\\ProductSchema' );
+    }
+
+    public function testCreateRecordAction()
+    {
+        $product = new Product\Model\Product;
+        ok($product);
+
+        $class = CRUD::generate('Product\\Model\\Product', 'Create');
+        ok($class);
+
+        $create = new $class( array( 'name' => 'A' ), $product);
+        ok($create);
+
+        $ret = $create->run();
+        ok($ret,'success action');
+
+        $product->delete();
+    }
+
+    public function testUpdateRecordAction()
+    {
+        $product = new Product\Model\Product;
+        ok($product);
+        $ret = $product->create(array( 
+            'name' => 'B',
+        ));
+        ok($ret->success,'record created.');
+
+        $class = CRUD::generate('Product\\Model\\Product', 'Update');
+        ok($class);
+
+        $update = new $class( array( 'id' => $product->id, 'name' => 'C' ), $product);
+        ok($update);
+
+        $ret = $update->run();
+        ok($ret,'success action');
+
+        $ret = $product->load(array( 'name' => 'C' ));
+        ok($ret->success);
+
+        $product->delete();
+    }
 
 }
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,15 @@
 <?php
 $loader = require 'vendor/autoload.php';
 $loader->add(null,'tests');
+
+function __() 
+{
+    $args = func_get_args();
+    $msg = _( array_shift( $args ) );
+    $id = 1;
+    foreach ($args as $arg) {
+        $msg = str_replace( "%$id" , $arg , $msg );
+        $id++;
+    }
+    return $msg;
+}
