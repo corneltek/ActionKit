1e22fec9221f220e35f083b6c8934d3053f7ee03
c9s <cornelius.howl@gmail.com>
2013-12-03 16:44:45 +0800

    renderList, renderItem

--- src/ActionKit/View/ManyToManyCheckboxView.php
+++ src/ActionKit/View/ManyToManyCheckboxView.php
@@ -25,39 +25,54 @@ class ManyToManyCheckboxView
         return $checked;
     }
 
+
+    /**
+     * Render list item
+     *
+     * @param string $relationId   The relationship identity
+     * @param BaseModel $item      Record object.
+     * @return Element('li')
+     */
+    public function renderItem($relationId, $item, $on = false) 
+    {
+        $id = $item->id;
+        $li       = new Element('li');
+        $label    = new Label;
+        $hiddenId = new HiddenInput("{$relationId}[{$id}][_foreign_id]", array( 'value' => $id ) );
+        $checkboxValue = $on ? 1 : 0;
+        $checkbox = new CheckboxInput("{$relationId}[{$id}][_connect]",array( 
+            'value' => $checkboxValue,
+        ));
+        if ( $on ) {
+            $checkbox->check();
+        }
+        $label->append( $checkbox );
+
+        $label->appendText( $item->dataLabel() );
+        $label->append( $hiddenId );
+        $li->append($label);
+        return $li;
+    }
+
     /**
      * Render a checkbox list base on the subset and superset collections.
      *
      * @param string $relationId the relationship id, used to render the form field key.
-     * @param BaseCollection[]   the related collection.
+     * @param BaseCollection[]   the related collection. (optional)
      * @param BaseCollection[]   the superset collection.
      */
-    public function _renderList($relationId, $subset, $superset) 
+    public function renderList($relationId, $subset, $superset) 
     {
         $ul = new Element('ul');
         $ul->addClass('actionkit-checkbox-view');
 
-        $checked = $this->_createCheckedMap($subset);
+        $checked = $subset ? $this->_createCheckedMap($subset) : array();
 
         // now we should render the superset, and set the checkbox to be 
         // connected from the $checked array.
         foreach( $superset as $item ) {
-            $id = $item->id;
-            $li       = new Element('li');
-            $label    = new Label;
-            $hiddenId = new HiddenInput(   "{$relationId}[{$id}][_foreign_id]", array( 'value' => $id ) );
-            $checkboxValue = ( isset($checked[$id]) ) ? 1 : 0;
-            $checkbox = new CheckboxInput( "{$relationId}[{$id}][_connect]",array( 
-                'value' => $checkboxValue,
-            ));
-            if ( $checkboxValue ) {
-                $checkbox->check();
-            }
-            $label->append( $checkbox );
-
-            $label->appendText( $item->dataLabel() );
-            $label->append( $hiddenId );
-            $li->append($label)->appendTo($ul);
+            $li = $this->renderItem( $relationId, $item, isset($checked[$id]) );
+            $li->appendTo($ul);
         }
         return $ul;
     }
@@ -71,7 +86,7 @@ class ManyToManyCheckboxView
      */
     public function render($relationId, $record, $collection)
     {
-        return $this->_renderList( $relationId, $record->{$relationId}, $collection );
+        return $this->renderList( $relationId, $record->id ? $record->{$relationId} : null , $collection );
         /**
          * $record: the main record
          * relationId: the relationship id
