125de78b5ce9786872fcbc99d3c8f4ceed576799
c9s <yoanlin93@gmail.com>
2014-04-24 17:59:03 +0800

    Improve record duplication

--- src/ActionKit/RecordAction/BulkCopyRecordAction.php
+++ src/ActionKit/RecordAction/BulkCopyRecordAction.php
@@ -1,16 +1,101 @@
 <?php
 namespace ActionKit\RecordAction;
+use Exception;
+
+function duplicate_file($from) {
+    $to = FileUtils::filename_increase($from);
+    copy($from , $to);
+    return $to;
+}
+
+/**
+ * Duplicate data based on the schema
+ */
+function duplicate_data($data, $schema) {
+    $newData = [];
+    foreach( $data as $name => $val ) {
+        if ( $val == null ) {
+            continue;
+        }
+        $column = $schema->getColumn($name);
+
+        switch( $column->contentType ) {
+        case "ImageFile":
+            $newData[ $name ] = duplicate_file($val);
+            break;
+        default:
+            $newData[ $name ] = $val;
+        }
+    }
+    if ( $pk = $schema->primaryKey ) {
+        unset($newData[$pk]);
+    }
+    return $newData;
+}
+
+function duplicate_record($record, $schema, $data = null) {
+    $data = $data ?: $record->getData();
+    $newData = duplicate_data($data, $schema);
+
+    $newRecord = $schema->newModel();
+    $ret = $newRecord->create($newData);
+    if (! $ret->success) {
+        throw new Exception($ret->message);
+    }
+    // XXX: check error
+
+    $relations = $schema->getRelations();
+    foreach( $relations as $rId => $relation ) {
+        if ( ! $relation->isHasMany() ) {
+            continue;
+        }
+
+        // var_dump( $relation ); 
+
+        $foreignRecord = $relation->newForeignModel();
+        $foreignColumnName = $relation['foreign_column'];
+
+        // fetch the related records
+        if ( isset($record->{ $rId }) ) {
+            $relatedRecords = $record->{ $rId };
+            $relatedRecords->fetch();
+
+            foreach( $relatedRecords as $relatedRecord ) {
+                $relatedRecordData = duplicate_data( $relatedRecord->getData() , $relatedRecord->getSchema() );
+                $relatedRecordData[ $foreignColumnName ] = $newRecord->id; // override the foreign column to the new record primary key
+                $ret = $foreignRecord->create($relatedRecordData);
+                if (! $ret->success) {
+                    throw new Exception($ret->message);
+                }
+                // XXX: check error
+            }
+
+        }
+    }
+    return $newRecord;
+}
+
+
 
 class BulkCopyRecordAction extends BulkRecordAction
 {
     const TYPE = 'bulk_copy';
 
+
+    /**
+     * @var array New fields are defined to be override.
+     */
     public $newFields = array('lang');
+
+    /**
+     * @var array unsetFields are defined to reset.
+     */
     public $unsetFields = array();
 
     public function schema()
     {
         foreach( $this->newFields as $field ) {
+            // XXX: initialize the label from schema
             $this->param($field);
         }
         parent::schema();
@@ -46,43 +131,32 @@ class BulkCopyRecordAction extends BulkRecordAction
 
     }
 
-    public function unsetPrimaryKey($schema, $data)
-    {
-        if ( $pk = $schema->primaryKey ) {
-            unset($data[$pk]);
-        }
-        return $data;
-    }
-
     public function run()
     {
         $newRecord = new $this->recordClass;
         $schema = $newRecord->getSchema();
 
+        $relations = $schema->getRelations();
+
+
         $newRecords = array();
         $records = $this->loadRecords();
         foreach($records as $record) {
-            $data = $record->getData();
+            $newData = $record->getData();
+            $newData = $this->beforeCopy($record, $newData);
 
-            $data = $this->unsetPrimaryKey($schema, $data);
-            
-            $data = $this->beforeCopy($record, $data);
-
-            $newRecord = new $this->recordClass;
-            $ret = $newRecord->create($data);
-
-            $newRecords[] = $newRecord;
-
-            if ( ! $ret->success ) {
-                return $this->error($ret->message);
-            }
-            if ( $result = $this->afterCopy($record, $data, $newRecord) ) {
-                return $result;
+            try {
+                $newRecord = duplicate_record($record, $schema, $newData);
+                if ( $result = $this->afterCopy($record, $newData, $newRecord) ) {
+                    return $result;
+                }
+                $newRecords[] = $newRecord;
+            } catch ( Exception $e ) {
+                return $this->error( $e->message );
             }
         }
-
         $this->finalize( $records, $newRecords );
-        return $this->success( count($records) . ' 個項目複製成功。');
+        return $this->success( count($newRecords) . ' 個項目複製成功。');
     }
 
 }
