5dfe0a6508e57c41808a0dff25425bbebfcc47ae
c9s <yoanlin93@gmail.com>
2015-06-16 16:00:20 +0800

    Add example

---
+++ example/test.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php';
+use ActionKit\Action;
+
+class LoginAction extends Action {
+
+    public function schema() {
+    }
+
+    public function run() {
+
+    }
+
+}
+
+
+
