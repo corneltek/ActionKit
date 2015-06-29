b51256b82bb3025265d40463dca8533a2f7a8e6f
azole <azolelai@gmail.com>
2015-06-29 22:34:43 +0800

    add Result test case

--- tests/ActionKit/ResultTest.php
+++ tests/ActionKit/ResultTest.php
@@ -1,22 +1,48 @@
 <?php
-use ActionKit\Action;
+use ActionKit\Result;
 
 class ResultTest extends PHPUnit_Framework_TestCase 
 {
     public function testResult()
     {
-        $login = new LoginTestAction;
-        $login->arg('username', 'Foo');
-
-        $username = $login->getParam('username');
-        $username->required = true;
-        $result = $login->runValidate();
-        $result = $login->getResult();
+        $result = new Result;
         ok($result);
 
-        is('error', $result->type);
-        is(false, $result->isSuccess());
-        is(true, $result->isError());
+        $result->success('Success Tset');
+        is('success', $result->type);
+        is(true, $result->isSuccess());
         ok($result->message);
+
+        ok($result->error('Error Tset'));
+        is('error', $result->type);
+        is(true, $result->isError());
+        ok($result->getMessage());
+
+        ok($result->valid('Valid Tset'));
+        is('valid', $result->type);
+        is(true, $result->isValidation());
+
+        ok($result->invalid('Valid Tset'));
+        is('invalid', $result->type);
+        is(true, $result->isValidation());
+
+        ok($result->completion('country', 'list', ['tw', 'jp', 'us']));
+        is('completion', $result->type);
+        is(true, $result->isCompletion());
+
+        ok($result->desc('description'));
+        ok($result->debug('debug'));
+
+        ok($result->toArray());
+        ok($result->__toString());
+    }
+
+    public function testResultData()
+    {
+        $result = new Result;
+        ok($result->data(['data1' => 'value1', 'data2' => 'value2']));
+        ok($result->data('data1', 'value1'));
+        ok($result->addData('data2', 'value2'));
+        ok($result->mergeData(['data3' => 'value3', 'data4' => 'value4']));
     }
 }
