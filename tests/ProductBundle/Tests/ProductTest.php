295354eea175eb87284c10ca394ebf581e618e18
c9s <yoanlin93@gmail.com>
2015-06-30 22:23:49 +0800

    Improve UpdateOrderingRecordAction test case

--- tests/ProductBundle/Tests/ProductTest.php
+++ tests/ProductBundle/Tests/ProductTest.php
@@ -1,54 +1,47 @@
 <?php
 use ActionKit\ActionRunner;
+use ActionKit\ServiceContainer;
 
 class ProductBundleTest extends PHPUnit_Framework_TestCase
 {
 
+    public function actionMapProvider() {
+        return [
+            ['ProductBundle\\Action\\UpdateProductImageOrdering', 'ProductBundle\\Model\\ProductImage'],
+            ['ProductBundle\\Action\\UpdateProductPropertyOrdering', 'ProductBundle\\Model\\ProductProperty'],
+            ['ProductBundle\\Action\\UpdateProductLinkOrdering', 'ProductBundle\\Model\\ProductLink'],
+            ['ProductBundle\\Action\\UpdateProductProductOrdering', 'ProductBundle\\Model\\ProductProduct'],
+            ['ProductBundle\\Action\\UpdateProductSubsectionOrdering', 'ProductBundle\\Model\\ProductSubsection'],
+        ];
+    }
+
+
+
     // TODO: 改成用 UpdateOrderingRecordActionTemplate 來建立 UpdateOrderingRecordAction
-    public function testProductUpdateOrderingActions() {
-        
+    /**
+     * @dataProvider actionMapProvider
+     */
+    public function testProductUpdateOrderingActions($actionClass, $recordClass) 
+    {
         $container = new ActionKit\ServiceContainer;
         $generator = $container['generator'];
         $generator->registerTemplate('FileBasedActionTemplate', new ActionKit\ActionTemplate\FileBasedActionTemplate());
 
         $runner = new ActionRunner($container);
-        ok($runner, 'action runner');
 
-        $sortActionClasses = [
-            'ProductBundle\\Action\\UpdateProductImageOrdering' => 'ProductBundle\\Model\\ProductImage',
-            'ProductBundle\\Action\\UpdateProductPropertyOrdering' =>  'ProductBundle\\Model\\ProductProperty',
-            'ProductBundle\\Action\\UpdateProductLinkOrdering' => 'ProductBundle\\Model\\ProductLink',
-            'ProductBundle\\Action\\UpdateProductProductOrdering' => 'ProductBundle\\Model\\ProductProduct',
-            'ProductBundle\\Action\\UpdateProductSubsectionOrdering' => 'ProductBundle\\Model\\ProductSubsection',
-        ];
+        $runner->registerAction('FileBasedActionTemplate', array(
+            'action_class' => $actionClass, 
+            'template' => '@ActionKit/RecordAction.html.twig', 
+            'variables' => [
+                'base_class' => 'ActionKit\\RecordAction\\UpdateOrderingRecordAction',
+                'record_class' => $recordClass,
+            ]
+        ));
+        $action = $runner->createAction($actionClass);
 
-
-        foreach( $sortActionClasses as $actionClass => $recordClass ) {
-            $runner->registerAction('FileBasedActionTemplate', array(
-                'action_class' => $actionClass, 
-                'template' => '@ActionKit/RecordAction.html.twig', 
-                'variables' => [
-                    'base_class' => 'ActionKit\\RecordAction\\UpdateOrderingRecordAction',
-                    'record_class' => $recordClass,
-                ]
-            ));
-        }
-        
-        foreach( $sortActionClasses as $actionClass => $recordClass ) {
-            $action = $runner->createAction($actionClass);
-            ok($action, $actionClass);
-        }
+        $this->assertNotNull($action);
     }
 
-
-    public function testProductRecordActions()
-    {
-
-
-        
-
-        
-    }
 }
 
 
