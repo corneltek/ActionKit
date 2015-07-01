a16f324d6e87c361ec8ead12a14328f9e60f9b79
c9s <yoanlin93@gmail.com>
2015-07-02 00:11:05 +0800

    Add type hinting to ActionException

--- src/ActionKit/Exception/ActionException.php
+++ src/ActionKit/Exception/ActionException.php
@@ -1,12 +1,13 @@
 <?php
 namespace ActionKit\Exception;
 use Exception;
+use ActionKit\Action;
 
 class ActionException extends Exception
 {
     public $action;
 
-    public function __construct($msg, $action)
+    public function __construct($msg, Action $action = null)
     {
         $this->action = $action;
         parent::__construct($msg);
