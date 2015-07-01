979afc34543337e7ac6217f28dcbb706fbbdac2b
c9s <yoanlin93@gmail.com>
2015-07-01 13:17:06 +0800

    Separate CodeGenActionTemplate tests to single test case

---
+++ tests/ActionKit/ActionTemplate/CodeGenActionTemplateTest.php
@@ -0,0 +1,55 @@
+<?php
+use ActionKit\ActionTemplate\CodeGenActionTemplate;
+use ActionKit\ActionRunner;
+use ActionKit\GeneratedAction;
+use ActionKit\Testing\ActionTestCase;
+
+class CodeGenActionTemplateTest extends ActionTestCase
+{
+
+
+    /**
+     * @dataProvider classNameProvider
+     */
+    public function testCodeGenTemplateActionSuccessfulGenerationWithExtra($className)
+    {
+        $actionTemplate = new CodeGenActionTemplate();
+        $runner = new ActionRunner;
+
+        $actionTemplate->register($runner, 'CodeGenActionTemplate', array(
+            'action_class' => $className,
+            'use' => ['TestApp\Database'],
+            'extends' => 'Action',
+            'constants' => [
+                'foo' => 123
+            ],
+        ));
+        $this->assertCount(1, $runner->getPretreatments());
+        $this->assertNotNull($pretreatment = $runner->getActionPretreatment($className));
+
+        $generatedAction = $actionTemplate->generate($className, $pretreatment['arguments']);
+        $this->assertRequireGeneratedAction($className, $generatedAction);
+    }
+
+
+    /**
+     * @dataProvider classNameProvider
+     */
+    public function testCodeGenTemplateActionSuccessfulGeneration($className)
+    {
+        $actionTemplate = new CodeGenActionTemplate();
+        $runner = new ActionRunner;
+
+        $actionTemplate->register($runner, 'CodeGenActionTemplate', array(
+            'action_class' => $className,
+            'extends' => 'Action',
+        ));
+        $this->assertCount(1, $runner->getPretreatments());
+        $this->assertNotNull($pretreatment = $runner->getActionPretreatment($className));
+
+        $generatedAction = $actionTemplate->generate($className, $pretreatment['arguments']);
+        $this->assertRequireGeneratedAction($className, $generatedAction);
+    }
+
+}
+
