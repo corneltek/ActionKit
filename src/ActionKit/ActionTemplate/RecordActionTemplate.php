13528447513e79b8d97f110b9f640d783be98070
c9s <yoanlin93@gmail.com>
2015-09-01 00:14:53 +0800

    when type is a string, cast it to type config by it's prefix

--- src/ActionKit/ActionTemplate/RecordActionTemplate.php
+++ src/ActionKit/ActionTemplate/RecordActionTemplate.php
@@ -17,11 +17,11 @@ class RecordActionTemplate extends CodeGenActionTemplate
      *        'namespace' => 'test',
      *        'model' => 'testModel',   // model's name
      *        'allowed_roles' => array('admin', 'manager'), 
-     *        'types' => array(
+     *        'types' => [
      *            ['prefix' => 'Create', 'allowed_roles' => ['user', 'admin'] ],
      *            ['prefix' => 'Update'],
      *            ['prefix' => 'Delete']
-     *        )
+     *        ]
      *    ));
      */
     public function register(ActionRunner $runner, $asTemplate, array $options = array())
@@ -42,7 +42,13 @@ class RecordActionTemplate extends CodeGenActionTemplate
             throw new RequiredConfigKeyException('types', 'types is an array of operation names for CRUD');
         }
 
-        foreach ( (array) $options['types'] as $type ) {
+        foreach ((array) $options['types'] as $type) {
+            // re-define type
+            if (is_string($type)) {
+                $type = [ 'prefix' => $type ];
+            }
+
+
             $actionClass = $options['namespace'] . '\\Action\\' . $type['prefix'] . $options['model'];
             $properties = ['recordClass' => $options['namespace'] . "\\Model\\" . $options['model']];
             $traits = array();
