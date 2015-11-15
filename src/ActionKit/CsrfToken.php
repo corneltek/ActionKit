e437c0d86f6c878bfbbee211c0718b4aa021437a
c9s <yoanlin93@gmail.com>
2015-11-15 14:05:02 +0800

    Add doc blocks

--- src/ActionKit/CsrfToken.php
+++ src/ActionKit/CsrfToken.php
@@ -7,21 +7,44 @@ class CsrfToken {
     public $sessionKey;
 
     /**
-     * @var int time to live of the token. using seconds.
+     * @var integer time to live of the token. using seconds.
      */
     public $ttl;
+
+    /**
+     * @var integer
+     */
     public $timestamp;
+
+
+    /**
+     * @var string
+     */
     public $salt;
+
+    /**
+     * @var string
+     */
     public $sessionId;
+
+    /**
+     * @var string
+     */
     public $ip;
+
+    /**
+     * @var string
+     */
     public $hash;
 
-    public function __construct($sessionKey, $ttl){
+    public function __construct($sessionKey, $ttl)
+    {
         $this->ttl = $ttl;
         $this->sessionKey = $sessionKey;
     }
 
-    public function checkExpiry($timestamp) {
+    public function checkExpiry($timestamp)
+    {
         return ($timestamp - $this->timestamp) < $this->ttl;
     }
 }
