1cc5a4a53acb2373069ca2a86ba9e25f8c1e5060
c9s <yoanlin93@gmail.com>
2015-11-21 19:49:46 +0800

    Fold constant

--- src/ActionKit/CsrfTokenProvider.php
+++ src/ActionKit/CsrfTokenProvider.php
@@ -18,7 +18,7 @@ class CsrfTokenProvider
      *
      * @param integer $ttl time to live
      */
-    public function generateToken($sessionKey = null, $ttl = 60 * 20)
+    public function generateToken($sessionKey = null, $ttl = 120)
     {
         $token = new CsrfToken($sessionKey ?: $this->sessionKey, $ttl);
         $token->timestamp = time();
