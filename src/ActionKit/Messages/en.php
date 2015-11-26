32c6152c1296b02d187550e93bbe6a7664a56344
c9s <yoanlin93@gmail.com>
2015-11-26 23:48:06 +0800

    Align messages

--- src/ActionKit/Messages/en.php
+++ src/ActionKit/Messages/en.php
@@ -1,25 +1,25 @@
 <?php
 return [
-    'file.required'  => 'File field %1 is required.',
-    'param.required' => 'Field %1 is required.',
-    'validation.error' => 'Please check your input.',
-    'csrf.token_expired' => 'csrf token is expired, please refresh your page.',
-    'csrf.token_mismatch' => 'CSRF Token mismatched.',
-    'csrf.token_invalid' => 'CSRF token invalid.',
+    'file.required'                         => 'File field %1 is required.',
+    'param.required'                        => 'Field %1 is required.',
+    'validation.error'                      => 'Please check your input.',
+    'csrf.token_expired'                    => 'csrf token is expired, please refresh your page.',
+    'csrf.token_mismatch'                   => 'CSRF Token mismatched.',
+    'csrf.token_invalid'                    => 'CSRF token invalid.',
 
     // record action messages
     'record_action.primary_key_is_required' => 'Updating record requires primary key value.',
-    'record_action.load_failed' => 'Can not load record.',
-    'record_action.record_not_found' => '%1 record not found.',
-    'record_action.validation_error' => '%1 validation failed.',
-    'record_action.successful_update' => '%1 record is updated successfully.',
-    'record_action.failed_update' => '%1 record update failed.',
+    'record_action.load_failed'             => 'Can not load record.',
+    'record_action.record_not_found'        => '%1 record not found.',
+    'record_action.validation_error'        => '%1 validation failed.',
+    'record_action.successful_update'       => '%1 record is updated successfully.',
+    'record_action.failed_update'           => '%1 record update failed.',
 
-    'bulk_delete.successful_delete' => '%1 items were deleted successfully.',
+    'bulk_delete.successful_delete'         => '%1 items were deleted successfully.',
 
-    'record_action.successful_create' => "%1 record is created successfully.",
-    'record_action.failed_create'     => "%1 record create failed.",
+    'record_action.successful_create'       => "%1 record is created successfully.",
+    'record_action.failed_create'           => "%1 record create failed.",
 
-    'record_action.successful_delete' => "%1 record is deleted successfully.",
-    'record_action.failed_delete'     => "%1 record delete failed.",
+    'record_action.successful_delete'       => "%1 record is deleted successfully.",
+    'record_action.failed_delete'           => "%1 record delete failed.",
 ];
