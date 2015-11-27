6db4d09055e50ce27c9d3295d2aebac9917c1827
c9s <yoanlin93@gmail.com>
2015-11-27 23:32:58 +0800

    Add IntType tests

--- src/ActionKit/ValueType/IntType.php
+++ src/ActionKit/ValueType/IntType.php
@@ -6,7 +6,7 @@ class IntType extends BaseType
 {
     public function test($value)
     {
-        return is_int($value);
+        return is_int($value) || filter_var($value, FILTER_VALIDATE_INT);
     }
 
     public function parse($value)
