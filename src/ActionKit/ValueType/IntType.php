23b6fad5a598f282ed18cb20fe6eda9404a3ab20
c9s <yoanlin93@gmail.com>
2016-03-18 17:10:14 +0800

    Fix int value testing (values from web form are string)

--- src/ActionKit/ValueType/IntType.php
+++ src/ActionKit/ValueType/IntType.php
@@ -6,7 +6,7 @@ class IntType extends BaseType
 {
     public function test($value)
     {
-        return is_int($value) || filter_var($value, FILTER_VALIDATE_INT);
+        return is_int($value) || filter_var($value, FILTER_VALIDATE_INT) || is_numeric($value);
     }
 
     public function parse($value)
