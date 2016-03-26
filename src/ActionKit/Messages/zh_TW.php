3c215125f1dd92948b69263eed64ed87e9fbc8ac
c9s <yoanlin93@gmail.com>
2016-03-26 20:41:47 +0900

    Fix zh-tw messages for csrf token

--- src/ActionKit/Messages/zh_TW.php
+++ src/ActionKit/Messages/zh_TW.php
@@ -3,10 +3,9 @@ return [
     'file.required'    => '請上傳檔案欄位 %1',
     'param.required'   => '請輸入欄位 %1',
     'validation.error' => '請檢查表單欄位是否正確。',
-    'csrf.token_expired' => '表單驗證逾期，請重新載入頁面',
-    'csrf.token_mismatch' => '表單驗證不符合',
-    'csrf.token_invalid' => '表單驗證錯誤.',
-
+    'csrf.token_expired' => '跨站請求認證碼逾期，請重新載入頁面',
+    'csrf.token_mismatch' => '跨站請求認證碼不符合，請重試',
+    'csrf.token_invalid' => '跨站請求認證碼不合法',
 
     'record_action.primary_key_is_required' => '需要鍵值',
     'record_action.load_failed' => '無法載入資料',
